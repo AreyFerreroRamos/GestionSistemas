4dfbb2d147f9a56600a6a9871b2987a5a0b50caa
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-02-22 23:06:02 +0100

    Arey: Script bfit.sh completament funcional.

--- Lab1/bfit.sh
+++ Lab1/bfit.sh
@@ -8,22 +8,28 @@
 #	Retorn:
 #		-
 
-if [ $# -eq 1 ]
+if [ $(id -u) -eq 0 ]
 then
-	if [ -f $1 ]
+	if [ $# -eq 1 ]
 	then
-		> $1
-		echo " $1" >> /var/log/syslog
-		exit 0
-	elif [ $1 = '-h' ]
-	then
-		echo -e "bfit.sh : Donat un fitxer, es buida i es registra en el fitxer de syslog la data i l'hora i el nom del fitxer que s'ha buïdat.\n\tParàmetres:\n\t\t- fitxer\n\tRetorn:\n\t\t-" >&2
-		exit 1
+		if [ -f $1 ]
+		then
+			> $1
+			logger "$1"	# Per comprovar que la tasca s'ha realitzat correctament es pot utilitzar la comanda 'tail -1 /var/log/syslog' com a superusuari.
+			exit 0
+		elif [ $1 = '-h' ]
+		then
+			echo -e "bfit.sh : Donat un fitxer, es buida i es registra en el fitxer de syslog la data i l'hora i el nom del fitxer que s'ha buïdat.\n\tParàmetres:\n\t\t- fitxer\n\tRetorn:\n\t\t-" >&2
+			exit 4
+		else
+			echo -e "El paràmetre introduït no és un fitxer." >&2
+			exit 3
+		fi
 	else
-		echo -e "El paràmetre introduït no és un fitxer." >&2
+		echo -e "El número de paràmetres és incorrecte." >&2
 		exit 2
 	fi
 else
-	echo -e "El número de paràmetres és incorrecte." >&2
-	exit 3
+	echo -e "L'usuari no és root." >&2
+	exit 1
 fi

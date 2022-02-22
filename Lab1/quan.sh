bc9b3159fa6f2bae7df189887d3cf48673fca80b
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-02-22 15:23:39 +0100

    Arey: Script quan.sh completament funcional.

--- Lab1/quan.sh
+++ Lab1/quan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Autor: Arey Ferrero Ramos
+# Autor: Arey Ferrero Ramos.
 # Data: 21 de febrer del 2022. Versió: 1.
 # Descripció: Donat un conjunt de paquets (separats per blanc), s'indica quins no están instal·lats i per aquells que estan instal·lats s'indica la data en que es van instal·lar.
 #	Paràmetres:
@@ -10,22 +10,23 @@
 
 if [ $# -eq 0 ]
 then
-	echo -e "Es requereix al menys un paquet com a paràmetre per dur a terme la funcionalitat." >&2
+	echo -e "Es requereix al menys un paquet com a paràmetre." >&2
 	exit 1
 elif [ $1 = '-h' ]
 then
-	echo -e "quan.sh : Donat un conjunt de paquets, s'indica quins no están instal·lats i per aquells que estan instal·lats s'indica la data en que es van instal·lar.\n\tParàmetres:\n\t\t- Llista de paquets (Separats per blanc).\n\tRetorn\n\t\t-" >&2
+	echo -e "quan.sh : Donat un conjunt de paquets, s'indica quins no estan instal·lats i per aquells que estan instal·lats s'indica la data en que es van instal·lar.\n\tParàmetres:\n\t\t- Llista de paquets (Separats per blanc).\n\tRetorn:\n\t\t-" >&2
 	exit 2
 else
 	IFS=$' '
 	for paquet in $*
 	do
-		linia=$(cat /var/log/dpkg.log* | grep "status installed $paquet")
-		if [ -z $linia ]
-		then
+		linia=$(cat /var/log/dpkg.log* | grep "status installed $paquet:")
+		if [[ -z $linia ]]
+	       	then
 			echo -e "El paquet '$paquet' no està instal·lat."
 		else
-			echo $linia | cut -f1,2 -d' '
+			data=$(echo $linia | cut -f1,2 -d' ')
+			echo -e "$paquet \t$data"
 		fi	
 	done
 	exit 0	

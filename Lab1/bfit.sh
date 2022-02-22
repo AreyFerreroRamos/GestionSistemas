67618e1624b4aa5df944f655775133ad54452bfe
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-02-22 21:41:02 +0100

    Arey: Primera versio de quins.sh

--- Lab1/bfit.sh
+++ Lab1/bfit.sh
@@ -3,5 +3,27 @@
 # Autor: Arey Ferrero Ramos.
 # Data: 21 de febrer del 2022. Versió: 1.
 # Descripció: Donat un fitxer, es buida i es registra en el fitxer de syslog la data i l'hora i el nom del fitxer que s'ha buïdat.
+#	Paràmetros:
+#		- fitxer
+#	Retorn:
+#		-
 
-
+if [ $# -eq 1 ]
+then
+	if [ -f $1 ]
+	then
+		> $1
+		echo " $1" >> /var/log/syslog
+		exit 0
+	elif [ $1 = '-h' ]
+	then
+		echo -e "bfit.sh : Donat un fitxer, es buida i es registra en el fitxer de syslog la data i l'hora i el nom del fitxer que s'ha buïdat.\n\tParàmetres:\n\t\t- fitxer\n\tRetorn:\n\t\t-" >&2
+		exit 1
+	else
+		echo -e "El paràmetre introduït no és un fitxer." >&2
+		exit 2
+	fi
+else
+	echo -e "El número de paràmetres és incorrecte." >&2
+	exit 3
+fi
--- Lab1/quins.sh
+++ Lab1/quins.sh
@@ -3,5 +3,30 @@
 # Autor: Arey Ferrero Ramos.
 # Data: 21 de febrer del 2022. Versió: 1.
 # Descripció: Donada una data i una hora, s'indica quins paquets han sigut instal·lats posteriorment.
+# 	Paràmetres:
+#		- Data
+#		- Hora
+#	Retorn:
+#		-
 
-
+if [ $# -eq 2 ]
+then
+	liniaFinal=$(cat /var/log/dpkg.log* | wc -l)
+	liniaInicial=$(cat /var/log/dpkg.log* | grep -n "$data $hora" | head -1 | cut -f1 -d':')
+	let numLinies=liniaFinal-liniaInicial
+	cat /var/log/dpkg.log* | tail -$numLinies
+	exit 0
+elif [ $# -eq 1 ]
+then
+	if [ $1 = '-h' ]
+	then
+		echo -e "Descripció: Donada una data i una hora, s'indica quins paquets han sigut instal·lats posteriorment.\n\tParàmetres:\n\t\t- Data\n\t\t- Hora\n\tRetorn:\n\t\t-" >&2
+		exit 1
+	else
+		echo -e "El número de paràmetres és incorrecte." >&2
+		exit 2
+	fi
+else
+	echo -e "El número de paràmetres és incorrecte." >&2
+	exit 2
+fi

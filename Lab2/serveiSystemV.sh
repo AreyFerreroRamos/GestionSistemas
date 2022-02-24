5318f956c8a5d56d21f19c7f575f0809e04d2d97
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-02-24 21:46:57 +0100

    Arey: Addició de la plantilla de l'Script serveiSystemV.sh

---
+++ Lab2/serveiSystemV.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Autor: Arey Ferrero Ramos.
+# Data: 28 de febrer del 2022. Versió: 1.
+# Descripció: Es fa una copia de seguretat comprimida del directori /var/log que es deixa sota un directori anomenat /back/logs i s'escriu un missatge de prioritat info en el syslog amb la data, hora actual i
+# 		mida del fitxer. El directori /back/logs es propietat de root, te permissos d'escriptura només per a root i permissos de lectura per a tots els usuaris.
+# 	Paràmetres:
+#		-
+#	Retorn:
+#		-
+
+

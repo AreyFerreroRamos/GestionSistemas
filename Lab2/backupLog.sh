4ed215501a2662ee0ae2e6b3de53536b748dfdf6
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-03-02 18:02:40 +0100

    Arey: Addició del servei i del timer que executen l'script per apagar el sistema.

--- Lab2/backupLog.sh
+++ Lab2/backupLog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 # Autor: Arey Ferrero Ramos.
-# Data: 28 de febrer del 2022. Versió: 1.
+# Data: 1 de març del 2022. Versió: 1.
 # Descripció: Es fa una copia de seguretat comprimida del directori /var/log que es deixa sota un directori anomenat /back/logs i s'escriu un missatge de prioritat info en el syslog amb la data, hora actual i
 # 		mida del fitxer. El directori /back/logs es propietat de root, te permisos d'escriptura només per a root i permisos de lectura per a tots els usuaris.
 # 	Paràmetres:
---
+++ Lab2/poweroff.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Autor: Arey Ferrero Ramos.
+# Data: 1 de març del 2022. Versió: 1.
+# Descripció: Conté la comanda per apagar el sistema segons un servei poweroff.service a una hora programada segons el timer del systemd poweroff.timer .
+
+systemctl poweroff

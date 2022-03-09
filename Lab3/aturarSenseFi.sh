bf78335f4316023df13e083f920a38018b9b7bd1
Arey Ferrero Ramos <arey.ferrero@estudiants.urv.cat>
2022-03-09 01:18:02 +0100

    Arey: Versió millorada dels scripts aturarSenseFi.sh i continuarSenseFi.sh.

--- Lab3/aturarSenseFi.sh
+++ Lab3/aturarSenseFi.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # Autor: Arey Ferrero Ramos.
-# Data: 7 de març del 2022. Versió: 1
+# Data: 8 de març del 2022. Versió: 1
 # Descripció: S'aturen una vegada els processos anomenats senseFi (Processos que s'han creat cada cert temps i en hores diferents i que consumeixen CPU i no acaben mai).
 #	Paràmetres:
 #		-
 # 	Retorn:
 #		-Fitxer amb els PIDs dels processos que es vagin aturant.
 
-IFS=$'\n'
-for proces in $(ps aux | grep senseFi)
-do
-	if [ $(echo $proces | tr -s ' ' | cut -f8 -d' ') = "R+" ]
-	then
-		pid=$(echo $proces | tr -s ' ' | cut -f2 -d' ')
-		kill -SIGSTOP $pid
-		echo $pid >> pidsProcessos.txt
-	fi
-done
+if [ $# -eq 0 ]
+then
+	IFS=$'\n'
+	for proces in $(ps aux | grep senseFi | head -n -1)
+	do
+		if [ $(echo $proces | tr -s ' ' | cut -f8 -d' ') = "R+" ]
+		then
+			pid=$(echo $proces | tr -s ' ' | cut -f2 -d' ')
+			kill -SIGSTOP $pid
+			echo $pid >> pidsProcessos.txt
+		fi
+	done
+elif [ $1 = "-h" ]
+then
+	echo -e "aturarSenseFi.sh: S'aturen una vegada els processos anomenats senseFi (Processos que s'han creat cada cert temps i en hores diferents i que consumeixen CPU i no acaben mai).\n\tParàmetres:\n\t\t-\n\tRetorn:\n\t\t-Fitxer amb els PIDs dels processos que es vagin aturant." >&2
+	exit 1
+else
+	echo -e "Error: Els paràmetres son incorrectes." >&2
+	exit 2
+fi
--- Lab3/continuarSenseFi.sh
+++ Lab3/continuarSenseFi.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
 # Autor: Arey Ferrero Ramos.
-# Data: 7 de març del 2022. Versió: 1
+# Data: 8 de març del 2022. Versió: 1
 # Descripció: A les 21:30 es reanuda l'execució d'un conjunt de processos recollits en un fitxer, amb una diferencia de temps de 30 segons entre cada un.
 #	Paràmetres:
 #		-Fitxer amb els PIDs dels processos que s'han anat aturant.
 # 	Retorn:
 #		-
 
-IFS=$'\n'
-for pid in $(cat $1)
-do
-	kill -SIGCONT $pid
-	sleep 10
-done
-
+if [ $# -eq 1 ]
+then
+	if [ -f $1 ]
+	then
+		IFS=$'\n'
+		for pid in $(cat $1)
+		do
+			kill -SIGCONT $pid
+			sleep 30
+		done
+	elif [ $1 = "-h" ]
+	then
+		echo -e "continuarSenseFi.sh: A les 21:30 es reanuda l'execució d'un conjunt de processos recollits en un fitxer, amb una diferencia de temps de 30 segons entre cada un.\n\tParàmetres:\n\t\t-Fitxer amb els PIDs dels processos que s'han anat aturant.\n\tRetorn:\n\t\t-" >&2
+		exit 1
+	else
+		echo -e "Error: El paràmetre '$1' és incorrecte." >&2
+		exit 2
+	fi
+else
+	echo -e "Error: El nombre de paràmetres és incorrecte." >&2
+	exit 3
+fi

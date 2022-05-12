949dec9d8be0ca9d4eab860167ccd8e45552eaa6
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-05-12 20:11:06 +0200

    Arey: Primer versió script 1 lab 6.

--- Lab6/crearMuntarDiscsVirtuals.sh
+++ Lab6/crearMuntarDiscsVirtuals.sh
@@ -2,9 +2,9 @@
 
 # Autor: Arey Ferrero Ramos.
 # Data: 17 de maig del 2022. Versió: 1.
-# Descripció: Es creen dos discs virtuals a memòria. El primer disc tindrà una mida de 100 MB, es muntarà al directori /usuari/home/Baixades cada vegada que l'usuari al que pertanyi entri en el sistema i 
-#		s'esborrarà quan l'usuari surti del sistema. Això implica que n'hi haurà un per cada usuari connectat. El segon disc tindrà format tmpfs es muntarà a /mnt/mem cada vegada que s'engegui la màquina
-#		i hi podran llegir i  escriure tots els usuaris del sistema.
+# Descripció: Es creen dos discs virtuals a memòria. El primer disc tindrà format tmpfs es muntarà a /mnt/mem cada vegada que s'engegui la màquina i hi podran llegir i  escriure tots els usuaris del sistema. 
+#		El segon disc tindrà una mida de 100 MB, es muntarà al directori /usuari/home/Baixades cada vegada que l'usuari al que pertanyi entri en el sistema i s'esborrarà quan l'usuari surti del sistema. 
+#		Això implica que n'hi haurà un per cada usuari connectat.
 #	Paràmetres:
 #		-
 #	Retorn:
@@ -14,7 +14,15 @@ if [ $(id -u) -eq 0 ]
 then
 	if [ $# -eq 0 ]
 	then
-
+		mkdir -p /mnt/mem
+		echo -e "tmpfs\t/mnt/mem\ttmpfs\tdefaults\t0\t0" >> /etc/fstab
+		for usuari in $(ls /home)
+		do
+			mkdir -p /home/$usuari/Baixades
+			echo -e "tmpfs\t/home/$usuari/Baixades\ttmpfs\tdefaults,user,noauto,size=100M\t0\t0" >> /etc/fstab
+			echo -e "mount /home/$usuari/Baixades" >> /etc/profile
+		done	
+		mount -a	# S'utiltza la comanda 'df -l' per comprovar que s'ha muntat el disc.
 		exit 0
 	elif [ $1 = "-h" ]
 	then

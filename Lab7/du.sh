9536197e3e908921dc06ef4fc09800cc4c29cdcf
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-05-19 00:54:50 +0200

    Arey: Creación esqueleto ejercicio 1 laboratorio 7.

---
+++ Lab7/du.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Autor: Arey Ferrero Ramos.
+# Data: 24 de maig del 2022. Versió: 1.
+# Descripció: S'utilitza la comanda 'du' per comprovar quant espai de disc ocupen els directoris d'entrada dels usuaris que estan sota el directori /home i que han entrat en el sistema en els últims tres dies.
+#	Paràmetres:
+#		-
+#	Retorn:
+#		-
+
+if [ $(id -u) -eq 0 ]
+then
+	if [ $# -eq 0 ]
+	then
+
+		exit 0
+	elif [ $1 = "-h" ]
+	then
+		echo -e "S'utilitza la comanda 'du' per comprovar quant espai de disc ocupen els directoris d'entrada dels usuaris que estan sota el directori /home i que han entrat en el sistema en els últims tres dies.\n\tParàmetres:\n\t\t-\n\tRetorn:\n\t\t-" >&2
+		exit 3
+	else
+		echo -e "Error: Els paràmetres son incorrectes." >&2
+		exit 2
+	fi
+else
+	echo -e "Error: No estàs executant com a root." >&2
+	exit 1
+fi

21ede11209c52877f3c679de992f1d7fa3c17f7b
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-05-18 02:49:49 +0200

    Arey: Ejercicio 3 y versión final. Lab 6.

--- Lab6/lp.sh
+++ Lab6/lp.sh
@@ -3,27 +3,33 @@
 # Autor: Arey Ferrero Ramos.
 # Data: 17 de maig del 2022. Versió: 1.
 # Descripció: Supleix la comanda lp en l'enviament d'un document a imprimir. S'incorpora la funcionalitat addicional de demanar la paraula clau (emmagatzemada al fitxer /etc/passwd) i comprovar la validesa. Si
-#		la clau es correcta, s'imprimeix i, si la clau és incorrecta, es mostra un missatge d'error. Cal utilitzar la comanda stty per introduir la paraula clau.
+#		la clau es correcta, s'imprimeix i, si la clau és incorrecta, es mostra un missatge d'error. S'utilitza la comanda stty per introduir la paraula clau.
 #	Paràmetre:
 #		-
 #	Retorn:
 #		-
 
-if [ $(id -u) -eq 0 ]
+if [ $# -eq 1 ]
 then
-	if [ $# -eq 0 ]
+	if [ $1 = "-h" ]
 	then
-		
-		exit 0
-	elif [ $1 = "-h" ]
-	then
-		echo -e "Supleix la comanda lp en l'enviament d'un document a imprimir. S'incorpora la funcionalitat addicional de demanar la paraula clau (emmagatzemada al fitxer /etc/passwd) i comprovar la validesa. Si la clau es correcta, s'imprimeix i, si la clau és incorrecta, es mostra un missatge d'error. Cal utilitzar la comanda stty per introduir la paraula clau.\n\tParàmetres:\n\t\t-\n\tRetorn:\n\t\t-" >&2
-		exit 3
-	else
-		echo -e "Error: Els paràmetres son incorrectes." >&2
+		echo -e "Supleix la comanda lp en l'enviament d'un document a imprimir. S'incorpora la funcionalitat addicional de demanar la paraula clau (emmagatzemada al fitxer /etc/passwd) i comprovar la validesa. Si la clau es correcta, s'imprimeix i, si la clau és incorrecta, es mostra un missatge d'error. S'utilitza la comanda stty per introduir la paraula clau.\n\tParàmetres:\n\t\t-\n\tRetorn:\n\t\t-" >&2
 		exit 2
+	else
+		echo "Password: "
+		stty -echo
+		read password
+		stty echo
+		if [ $(sudo cat /etc/shadow | grep $USER | cut -f2 -d':') = $(openssl passwd -6 -salt $(sudo cat /etc/shadow | grep $USER | cut -f3 -d'$') $password) ]
+		then
+			lp $1
+			exit 0
+		else
+			echo -e "Error: La paraula clau introduïda és incorrecta." >&2
+			exit 3
+		fi
 	fi
 else
-	echo -e "Error: No estàs executant com a root." >&2
+	echo -e "Error: El nombre de paràmetres és incorrecte." >&2
 	exit 1
 fi
